1b373335a596cdcae1ea87b2c91eb75f4f00cd16
divyanshmathur1804 <44617450+divyanshmathur1804@users.noreply.github.com>
2023-04-19 23:49:22 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,16 @@
+
+        $(document).ready(function(){
+
+$('.datepicker').datepicker({
+    format: 'dd-mm-yyyy',
+    autoclose: true,
+    startDate: '0d'
+});
+
+$('.cell').click(function(){
+    $('.cell').removeClass('select');
+    $(this).addClass('select');
+});
+
+});
+    
